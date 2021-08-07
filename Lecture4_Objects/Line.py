2ffbbb9fcf21ade0de2801b444c5d00b90baad94
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-07 10:15:18 +0300

    Line class is added

---
+++ Lecture4_Objects/Line.py
@@ -0,0 +1,20 @@
+
+
+class Line:
+
+    def __init__(self,x1=0,y1=0,x2=1,y2=1):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def length(self):
+        # d=√((x_2-x_1)²+(y_2-y_1)²) => Line Length Equation 2 Points
+        return ((self.x1-self.x2)**2+(self.y1-self.y2)**2)**(1/2)
+
+    def slope(self):
+        return (self.y1-self.y2)/(self.x1-self.x2)
+
+    def moveX(self,addX=0,addY=0):
+        self.x1 += addX
+        self.y1 += addY
