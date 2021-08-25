1f9b4211e9e84634eb744df5382ed62f2b508f25
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-25 22:30:51 +0300

    End of struggling

--- DenizUcgunPY/CarSimulation.py
+++ DenizUcgunPY/CarSimulation.py
@@ -24,8 +24,13 @@ else:
     carkm += maxKm
     carfuel = 0
     print("There isn't enough fuel! Go to the nearest gas station!")
+    
 ## Get a new passanger named "Kagan" there -> Tesla
-passengers.append("Kagan")
+if len(passengers) < 3:    
+    passengers.append("Kagan")
+else:
+    print("There is no space !!")
+
 ## Move further 900 km
 if (900 * 1.6 <= carfuel):
     carkm += 900
@@ -40,17 +45,40 @@ else:
 ##################
 
 ## Move 22 km with
-carkm2 += 22
-carfuel2 -= 22 * 5.6
+if (22 * 5.6 <= carfuel2):
+    carkm2 += 22
+    carfuel2 -= 22 * 5.6
+else:
+    maxKm = carfuel2 / 5.6
+    print("maxkm: ",maxKm)
+    carkm2 += maxKm
+    carfuel2 = 0
+    print("There isn't enough fuel! Go to the nearest gas station!")
 ## Get 26 lt fuel
 carfuel2 += 26
 ## Move 17 km 
-carkm2 += 17
-carfuel2 -= 17 * 5.6
+if (22 * 5.6 <= carfuel2):
+    carkm2 += 17
+    carfuel2 -= 17 * 5.6
+else:
+    maxKm = carfuel2 / 5.6
+    carkm2 += maxKm
+    print("maxkm: ",maxKm)
+    carfuel2 = 0
+    print("There isn't enough fuel! Go to the nearest gas station!")
 ## Take 3 passangers -> "Mehmet" "Hatice" "Muhammet"
-passengers2.append("Mehmet")
-passengers2.append("Hatice")
-passengers2.append("Muhammet")
+if len(passengers2) < 3:
+    passengers2.append("Mehmet")
+else:
+    print("There is no space !!")
+if len(passengers2) < 3:
+    passengers2.append("Hatice")
+else:
+    print("There is no space !!")
+if len(passengers2) < 3:    
+    passengers2.append("Muhammet")
+else:
+    print("There is no space !!")
 
 print(f'--- Car {carName} ---\n| km: {carkm}\n| fuel: {carfuel}\n| Passengers: {passengers}')
 print(f'--- Car {carName2} ---\n| km: {carkm2}\n| fuel: {carfuel2}\n| Passengers: {passengers2}')
