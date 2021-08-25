c457b37ea00627686a41d8a430db83f98bc57622
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-25 22:55:53 +0300

    removed prev version

--- DenizUcgunPY/CarSimulation.py
+++ DenizUcgunPY/CarSimulation.py
@@ -47,23 +47,6 @@ class Car:
             for passanger in passengers:
                 self.get_passenger(passanger)
 
-
-
-
-carName = "Tesla Car"
-carkm = 0.0   # km
-carfuel = 100 # kWh
-consumption = 1.6 # kWh/km
-passengers = ["Emin","Deniz"] # Max 4
-
-
-
-carName2 = "Ferrari"
-carkm2 = 0.0   # km
-carfuel2 = 120  # lt
-consumption2 = 5.6 # lt/km
-passengers2 = ["Durmus","Ezgi"] # Max 3
-
 car1 = Car("Tesla Car",100,1.6,["Emin","Deniz"],4)
 car2 = Car("Ferrari",120,5.6,["Durmus","Ezgi"],3)
 car3 = Car("Mustang GT",220,11,["Umut"],2)
@@ -72,86 +55,18 @@ car1.print_car()
 car2.print_car()
 car3.print_car()
 
-
-print(f'--- Car {carName} ---\n| km: {carkm}\n| fuel: {carfuel}\n| Passengers: {passengers}')
-print(f'--- Car {carName2} ---\n| km: {carkm2}\n| fuel: {carfuel2}\n| Passengers: {passengers2}')
-
-## Move 10 km with Tesla
 car1.move(10)
-# if (10 * consumption <= carfuel):
-#     carkm += 10
-#     carfuel -= 10 * consumption
-# else:
-#     maxKm = carfuel / consumption
-#     carkm += maxKm
-#     carfuel = 0
-#     print("There isn't enough fuel! Go to the nearest gas station!")
-    
-## Get a new passanger named "Kagan" there -> Tesla
 car1.get_passenger("Kagan")
-# if len(passengers) < 3:    
-#     passengers.append("Kagan")
-# else:
-#     print("There is no space !!")
-
-## Move further 900 km
 car1.move(900)
-# if (900 * consumption <= carfuel):
-#     carkm += 900
-#     carfuel -= 900 * consumption
-# else:
-#     maxKm = carfuel / consumption
-#     print("Maxkm: ",maxKm)
-#     carkm += maxKm
-#     carfuel = 0
-#     print("There isn't enough fuel! Go to the nearest gas station!")
 
 ##################
-
-## Move 22 km with
 car2.move(22)
-# if (22 * consumption2 <= carfuel2):
-#     carkm2 += 22
-#     carfuel2 -= 22 * consumption2
-# else:
-#     maxKm = carfuel2 / consumption2
-#     print("maxkm: ",maxKm)
-#     carkm2 += maxKm
-#     carfuel2 = 0
-#     print("There isn't enough fuel! Go to the nearest gas station!")
-## Get 26 lt fuel
-car2.get_fuel(26)
-# carfuel2 += 26
-## Move 17 km 
+car2.get_fuel(26) 
 car2.move(17)
-# if (22 * consumption2 <= carfuel2):
-#     carkm2 += 17
-#     carfuel2 -= 17 * consumption2
-# else:
-#     maxKm = carfuel2 / consumption2
-#     carkm2 += maxKm
-#     print("maxkm: ",maxKm)
-#     carfuel2 = 0
-#     print("There isn't enough fuel! Go to the nearest gas station!")
-## Take 3 passangers -> "Mehmet" "Hatice" "Muhammet"
 car2.get_passengers(["Mehmet","Hatice","Muhammet"])
-# if len(passengers2) < 3:
-#     passengers2.append("Mehmet")
-# else:
-#     print("There is no space !!")
-# if len(passengers2) < 3:
-#     passengers2.append("Hatice")
-# else:
-#     print("There is no space !!")
-# if len(passengers2) < 3:    
-#     passengers2.append("Muhammet")
-# else:
-#     print("There is no space !!")
 
 car1.print_car()
 car2.print_car()
-# print(f'--- Car {carName} ---\n| km: {carkm}\n| fuel: {carfuel}\n| Passengers: {passengers}')
-# print(f'--- Car {carName2} ---\n| km: {carkm2}\n| fuel: {carfuel2}\n| Passengers: {passengers2}')
 
 
 
