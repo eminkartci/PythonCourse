fbc63d1d25c15b5ddc02ab760bbab477e7441c84
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-25 22:54:23 +0300

    replaced

--- DenizUcgunPY/CarSimulation.py
+++ DenizUcgunPY/CarSimulation.py
@@ -28,6 +28,26 @@ class Car:
             self.fuel = 0
             print(f"Car {self.name} cannot move further. Please go to the nearest gas station!")
 
+    def get_passenger(self,passanger):
+        if (self.capacity > len(self.passengers)):
+            # get passenger
+            self.passengers.append(passanger)
+        else:
+            # cannot
+            print(f"There is no space for Passenger {passanger}")
+
+    def get_fuel(self,newFuel):
+        self.fuel += newFuel
+
+    def get_passengers(self,passengers):
+        if len(passengers) < 2:
+            # warn
+            print("Please use approprate function")
+        else:
+            for passanger in passengers:
+                self.get_passenger(passanger)
+
+
 
 
 carName = "Tesla Car"
@@ -68,62 +88,70 @@ car1.move(10)
 #     print("There isn't enough fuel! Go to the nearest gas station!")
     
 ## Get a new passanger named "Kagan" there -> Tesla
-if len(passengers) < 3:    
-    passengers.append("Kagan")
-else:
-    print("There is no space !!")
+car1.get_passenger("Kagan")
+# if len(passengers) < 3:    
+#     passengers.append("Kagan")
+# else:
+#     print("There is no space !!")
 
 ## Move further 900 km
-if (900 * consumption <= carfuel):
-    carkm += 900
-    carfuel -= 900 * consumption
-else:
-    maxKm = carfuel / consumption
-    print("Maxkm: ",maxKm)
-    carkm += maxKm
-    carfuel = 0
-    print("There isn't enough fuel! Go to the nearest gas station!")
+car1.move(900)
+# if (900 * consumption <= carfuel):
+#     carkm += 900
+#     carfuel -= 900 * consumption
+# else:
+#     maxKm = carfuel / consumption
+#     print("Maxkm: ",maxKm)
+#     carkm += maxKm
+#     carfuel = 0
+#     print("There isn't enough fuel! Go to the nearest gas station!")
 
 ##################
 
 ## Move 22 km with
-if (22 * consumption2 <= carfuel2):
-    carkm2 += 22
-    carfuel2 -= 22 * consumption2
-else:
-    maxKm = carfuel2 / consumption2
-    print("maxkm: ",maxKm)
-    carkm2 += maxKm
-    carfuel2 = 0
-    print("There isn't enough fuel! Go to the nearest gas station!")
+car2.move(22)
+# if (22 * consumption2 <= carfuel2):
+#     carkm2 += 22
+#     carfuel2 -= 22 * consumption2
+# else:
+#     maxKm = carfuel2 / consumption2
+#     print("maxkm: ",maxKm)
+#     carkm2 += maxKm
+#     carfuel2 = 0
+#     print("There isn't enough fuel! Go to the nearest gas station!")
 ## Get 26 lt fuel
-carfuel2 += 26
+car2.get_fuel(26)
+# carfuel2 += 26
 ## Move 17 km 
-if (22 * consumption2 <= carfuel2):
-    carkm2 += 17
-    carfuel2 -= 17 * consumption2
-else:
-    maxKm = carfuel2 / consumption2
-    carkm2 += maxKm
-    print("maxkm: ",maxKm)
-    carfuel2 = 0
-    print("There isn't enough fuel! Go to the nearest gas station!")
+car2.move(17)
+# if (22 * consumption2 <= carfuel2):
+#     carkm2 += 17
+#     carfuel2 -= 17 * consumption2
+# else:
+#     maxKm = carfuel2 / consumption2
+#     carkm2 += maxKm
+#     print("maxkm: ",maxKm)
+#     carfuel2 = 0
+#     print("There isn't enough fuel! Go to the nearest gas station!")
 ## Take 3 passangers -> "Mehmet" "Hatice" "Muhammet"
-if len(passengers2) < 3:
-    passengers2.append("Mehmet")
-else:
-    print("There is no space !!")
-if len(passengers2) < 3:
-    passengers2.append("Hatice")
-else:
-    print("There is no space !!")
-if len(passengers2) < 3:    
-    passengers2.append("Muhammet")
-else:
-    print("There is no space !!")
+car2.get_passengers(["Mehmet","Hatice","Muhammet"])
+# if len(passengers2) < 3:
+#     passengers2.append("Mehmet")
+# else:
+#     print("There is no space !!")
+# if len(passengers2) < 3:
+#     passengers2.append("Hatice")
+# else:
+#     print("There is no space !!")
+# if len(passengers2) < 3:    
+#     passengers2.append("Muhammet")
+# else:
+#     print("There is no space !!")
 
-print(f'--- Car {carName} ---\n| km: {carkm}\n| fuel: {carfuel}\n| Passengers: {passengers}')
-print(f'--- Car {carName2} ---\n| km: {carkm2}\n| fuel: {carfuel2}\n| Passengers: {passengers2}')
+car1.print_car()
+car2.print_car()
+# print(f'--- Car {carName} ---\n| km: {carkm}\n| fuel: {carfuel}\n| Passengers: {passengers}')
+# print(f'--- Car {carName2} ---\n| km: {carkm2}\n| fuel: {carfuel2}\n| Passengers: {passengers2}')
 
 
 
