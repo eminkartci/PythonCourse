e6d5d16e5858220a155d8cd3450c2537a53bd53c
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-25 22:10:47 +0300

    2 different cars are implemented

--- DenizUcgunPY/CarSimulation.py
+++ DenizUcgunPY/CarSimulation.py
@@ -1,2 +1,18 @@
 
+carName = "Tesla Car"
+carkm = 0.0   # km
+carfuel = 100 # kWh
+consumption = 1.6 # kWh/km
+passengers = ["Emin","Deniz"] # Max 4
+
+carName2 = "Ferrari"
+carkm2 = 0.0   # km
+carfuel2 = 120  # lt
+consumption2 = 5.6 # lt/km
+passengers2 = ["Durmus","Ezgi"] # Max 3
+
+
+
+
+
 
