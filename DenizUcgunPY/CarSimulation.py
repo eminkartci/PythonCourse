b4971aecd029ccf7aeb9de44eaaabd6fd9908d87
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-25 22:17:09 +0300

    Action items are executed for cars

--- DenizUcgunPY/CarSimulation.py
+++ DenizUcgunPY/CarSimulation.py
@@ -11,8 +11,27 @@ carfuel2 = 120  # lt
 consumption2 = 5.6 # lt/km
 passengers2 = ["Durmus","Ezgi"] # Max 3
 
+## Move 10 km with Tesla
+carkm += 10
+carfuel -= 10 * 1.6
+## Get a new passanger named "Kagan" there -> Tesla
+passengers.append("Kagan")
+## Move further 900 km
+carkm += 900
+carfuel -= 900 * 1.6
 
+##################
 
-
-
+## Move 22 km with
+carkm2 += 22
+carfuel2 -= 22 * 5.6
+## Get 26 lt fuel
+carfuel += 26
+## Move 17 km 
+carkm2 += 17
+carfuel2 -= 17 * 5.6
+## Take 3 passangers -> "Mehmet" "Hatice" "Muhammet"
+passengers2.append("Mehmet")
+passengers2.append("Hatice")
+passengers2.append("Muhammet")
 
