2c39ece3be7c238dcf5a626e5d2f10cd9c31c7aa
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-25 22:24:59 +0300

    For each and for

--- QuickPython/inputTrial.py
+++ QuickPython/inputTrial.py
@@ -2,13 +2,22 @@ import sys
 
 print('Bu Program calisiyor!')
 
-for index in range(len(sys.argv)):
+# for index in range(len(sys.argv)):
 
-    if index == 0:
-        continue
+#     if index == 0:
+#         continue
 
-    print(f"Index: {index}")
-    print(f"Value: {sys.argv[index]}")
+#     print(f"Index: {index}")
+#     print(f"Value: {sys.argv[index]}")
 
+# print("Emin","Basak","Deniz","Muart","Nihan",sep="\n")
+
+isimler = ["Emin","Basak","Deniz"]
+
+for isim in isimler:
+    print(isim)
+
+for i in range(len(isimler)):
+    print(isimler[i])
 
 
