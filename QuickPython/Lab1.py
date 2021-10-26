8c71003546054c353110dad6f4a539b5906e1703
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-26 11:41:34 +0300

    Question 2 is done

--- QuickPython/Lab1.py
+++ QuickPython/Lab1.py
@@ -3,12 +3,38 @@
 
 
 # Question 1
-name = "Emin"
-content = f"""
- /\\_/\\
-/ oo  \\ 
-\\  ~  / 
-Hello “{name}”!
-"""
+#################
+# name = "Emin"
+# content = f"""
+#  /\\_/\\
+# / oo  \\ 
+# \\  ~  / 
+# Hello “{name}”!
+# """
+#################
 
-print(content)
+# print(content)
+
+# Question 2
+#################
+# s0 = 12
+# v0 = 3.5
+# a = 9.8
+# t = 10
+
+# s0,v0,a,t = 12,3.5,9.8,10
+
+# S = s0 + v0 * t + 1/2 * a * (t**2)
+
+# print(f"The value of s is {S}")
+#################
+
+## Question 3
+
+#################
+
+
+
+#################
+
+
